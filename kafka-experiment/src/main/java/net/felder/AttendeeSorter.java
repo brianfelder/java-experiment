10d08bc52e548c5972be7473c8c9c7f7962d2add
Felder, Brian <bfelder@cvent.com>
2017-06-22 15:48:20 -0700

    Aggregated stream by first name

--- kafka-experiment/src/main/java/net/felder/AttendeeSorter.java
+++ kafka-experiment/src/main/java/net/felder/AttendeeSorter.java
@@ -1,5 +1,6 @@
 package net.felder;
 
+import com.google.common.base.Strings;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -9,7 +10,6 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.Aggregator;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.apache.kafka.streams.kstream.KTable;
@@ -26,20 +26,20 @@ public class AttendeeSorter {
 
     public static void main(String... args) throws Exception {
         createAndStartStreams();
-        consumeFromTopicHowManyRecords(Constants.ATTENDEES_BY_FIRST_NAME_TOPIC, 5);
+        consumeFromTopicHowManyRecords(Constants.ATTENDEES_BY_FIRST_NAME_TOPIC, 10000);
         closeStreams();
     }
 
     public static void consumeFromTopicHowManyRecords(String topicName, int recordCount) {
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProperties());
+        KafkaConsumer<String, Object> consumer = new KafkaConsumer<>(consumerProperties());
 
         consumer.subscribe(Arrays.asList(topicName));
 
         int consumedCount = 0;
         while (consumedCount < recordCount) {
-            ConsumerRecords<String, String> records = consumer.poll(1000);
+            ConsumerRecords<String, Object> records = consumer.poll(1000);
             consumedCount += records.count();
-            for (ConsumerRecord<String, String> record : records) {
+            for (ConsumerRecord<String, Object> record : records) {
                 System.out.println("Received:" + record.key() + " - " + record.value());
             }
         }
@@ -50,17 +50,36 @@ public class AttendeeSorter {
         KStreamBuilder builder = new KStreamBuilder();
 
         KStream<String, String> source = builder.stream(Constants.ATTENDEE_INPUT_TOPIC);
-        KTable<String, String> attendeesByFirstNameTable = source
+
+        // Concatenate names onto the end of the key.
+        /*
+        KTable<String, String> attendeesByFirstNameConcatenateTable = source
                 .map((key, value) -> {
                     KeyValue<String, String> toReturn = new KeyValue<>(NameUtils.firstNameFrom(value), value);
                     return toReturn;
                 })
                 .groupByKey()
-                .reduce((aggValue, newValue) -> aggValue + ":::" + newValue, "attendees-by-first-name");
-        KStream<String, String> attendeesByFirstName = attendeesByFirstNameTable.toStream();
-        attendeesByFirstName.to(Constants.ATTENDEES_BY_FIRST_NAME_TOPIC);
+                .reduce((aggValue, newValue) -> aggValue + ":::" + newValue, "attendees-by-first-name-aggregated");
+        KStream<String, String> attendeesByFirstNameConcat = attendeesByFirstNameConcatenateTable.toStream();
+        attendeesByFirstNameConcat.to(Constants.ATTENDEES_BY_FIRST_NAME_TOPIC);
+        */
 
-        streams = new KafkaStreams(builder, streamProperties());
+        // Value is sum of the instances of the name.
+        KTable<String, String> attendeesByFirstNameSumTable = source
+                // Make the first name the key.
+                .map((key, value) -> {
+                    KeyValue<String, String> toReturn = new KeyValue<>(NameUtils.firstNameFrom(value), value);
+                    return toReturn;
+                })
+                // Group and count the result.
+                .groupByKey()
+                .count("attendees-by-first-name-sum")
+                // Convert values to String for processing.
+                .mapValues((value) -> Strings.padStart(value.toString(), 4, '0'));
+        KStream<String, String> attendeesByFirstNameSum = attendeesByFirstNameSumTable.toStream();
+        attendeesByFirstNameSum.to(Constants.ATTENDEES_BY_FIRST_NAME_TOPIC);
+
+        streams = new KafkaStreams(builder, streamCountProperties());
         streams.start();
     }
 
@@ -68,11 +87,12 @@ public class AttendeeSorter {
         streams.close();
     }
 
-    public static Properties streamProperties() {
+    public static Properties streamCountProperties() {
         Properties toReturn = new Properties();
         toReturn.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-attendees" + UUID.randomUUID());
         toReturn.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         toReturn.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        // Using Long value serde.
         toReturn.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         toReturn.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         toReturn.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
@@ -87,19 +107,5 @@ public class AttendeeSorter {
         toReturn.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         toReturn.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return toReturn;
-    }
-
-    private static class CountAggregator<K, V, T> implements Aggregator<String, Integer, Integer> {
-
-        @Override
-        public Integer apply(String aggregateKey, Integer newValue, Integer aggregateValue) {
-            Integer toReturn = aggregateValue + newValue;
-            System.out.println(String.format("aggregateKey: %s newValue: %d aggregateValue: %d toReturn: %d",
-                    aggregateKey,
-                    newValue,
-                    aggregateValue,
-                    toReturn));
-            return toReturn;
-        }
     }
 }
